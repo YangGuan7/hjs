3653950f45c445fbee719446ea27fcb328abe8b2
GeoVector <qaztw030@gmail.com>
2024-12-03 14:01:13 +0800

    1401

--- my-app/pages/song.js
+++ my-app/pages/song.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
+import LocomotiveScroll from 'locomotive-scroll';
 
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
 export default function song() {
   return (
     <>
-      <div>MEEEEEEEEEE</div>
-      <div className={styles["main-page-title"]}>
-        <PageTitle chineseTitle="找市集" englishTitle="Search Market　" />
-        <SearchMarketSwitch />
-      </div>
+      <div data-scroll-container>
+        <div data-scroll-section>
+            <h1 data-scroll>Hey</h1>
+            <p data-scroll>👋</p>
+        </div>
+        <div data-scroll-section>
+            <h2 data-scroll data-scroll-speed="1">What's up?</h2>
+            <p data-scroll data-scroll-speed="2">😬</p>
+        </div>
+    </div>
     </>
   );
 }
